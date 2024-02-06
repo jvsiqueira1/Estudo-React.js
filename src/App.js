8ad5db1895c691d13289c256ff37527bf786daf1
João Vitor de Siqueira Campos <vitor.siqueira@centralit.com.br>
2024-02-06 14:44:01 -0400

    Termino das aulas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import "./App.css";
-import Evento from "./components/Evento";
-import Form from "./components/Form";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Empresa from "./pages/Empresa";
+import Contato from "./pages/Contato";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento />
-      <Form />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/empresa" element={<Empresa />}></Route>
+        <Route path="/contato" element={<Contato />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
